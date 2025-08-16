1c9bfcfb91bf6d3bbb69ea5b7fd98996250f702c
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-08-16 13:40:36 +0530

    palindrome string

---
+++ Java/src/PalindromeString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static boolean isPalindrome(String s){
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String : ");
+        String s = sc.nextLine();
+        System.out.println(PalindromeStringFunc(s));
+    }
+    public static String PalindromeStringFunc(String s){
+        char[] ch  = s.toCharArray();
+        int st = 0 , end = ch.length-1;
+        while(st<end){
+            char temp = ch[st];
+            ch[st] = ch[end];
+            ch[end] = temp;
+            st++;
+            end--;
+        }
+        return String.valueOf(ch);
+    }
+
+}
