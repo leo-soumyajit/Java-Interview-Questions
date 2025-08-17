fb8452e1d326e3fd24f63c0ac518e3dd00eb6f95
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-08-17 14:36:20 +0530

    rev words and rev each words in string

---
+++ Java/src/RevEachWordInAString.java
@@ -0,0 +1,36 @@
+public class RevEachWordInAString {
+
+    public static void ReverseWithLibrary(String str){
+        String[] s = str.split("\\s+");
+        StringBuilder output = new StringBuilder();
+
+        for(String word : s){
+            StringBuilder sb = new StringBuilder(word);
+            output.append(sb.reverse()).append(" ");
+        }
+
+
+        System.out.println(output.toString().trim());
+    }
+
+    public static void ReverseWithOutLibrary(String str){
+        String[] s = str.split("\\s+");
+        StringBuilder output = new StringBuilder();
+
+        for(String word : s){
+           for (int i = word.length()-1 ; i>=0 ; i--) {
+               output.append(word.charAt(i));
+           }
+           output.append(" ");
+        }
+        System.out.println(output.toString().trim());
+    }
+
+    public static void main(String[] args) {
+        ReverseWithLibrary("Java is good programming langauges");
+        ReverseWithOutLibrary("Java is good programming langauges");
+    }
+
+
+
+}
