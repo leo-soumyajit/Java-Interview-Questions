6b78bc256b72494d14e4eb9b1acee8ec7ca61760
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-09-06 01:37:19 +0530

    First Missing Positive in Array

---
+++ Java/src/FirstMissingPositive.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class FirstMissingPositive {
+    public static int missingFPositive(int[] arr){
+        int res = 1;
+        Arrays.sort(arr);
+        for (int i = 0 ; i< arr.length ; i++){
+            if(arr[i]==res)res++;
+            else if(res < arr[i])break;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7,8,9,11,12};
+        System.out.println(missingFPositive(arr));
+
+    }
+}
