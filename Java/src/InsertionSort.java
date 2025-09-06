bc5c0bfba2180c8e93d608050ae78e3bf8290c0a
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-09-06 15:03:36 +0530

    Insertion sort algo

---
+++ Java/src/InsertionSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr){
+        for(int i = 1 ; i< arr.length-1; i++){
+            int j = i;
+            while(j>0 && arr[j] < arr[j-1]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,1,5};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
