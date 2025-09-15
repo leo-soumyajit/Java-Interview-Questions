8d442b9f0c776ba4410ae7f72b3ddce53286a128
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-09-15 22:38:25 +0530

    pivot index program

---
+++ Java/src/PivotIndex.java
@@ -0,0 +1,23 @@
+public class PivotIndex {
+    public static void main(String[] args) {
+        int[] arr = {1,7,3,6,5,6};
+        System.out.println(pivot(arr)); //expected 3;
+
+    }
+    public static int pivot(int[] arr){
+        int sum = 0;
+        for(int i = 0;i< arr.length ; i++){
+            sum += arr[i];
+        }
+        int left = 0;
+        int right = sum;
+        for(int i = 0 ; i< arr.length ; i++){
+            left += arr[i];
+            if(left == right){
+                return i;
+            }
+            right-=arr[i];
+        }
+        return -1;
+    }
+}
