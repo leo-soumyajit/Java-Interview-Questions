f6c10a37c641f1093c33b2d3ae928404887ad0c4
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-09-25 00:14:49 +0530

    fix error in insertion sort

--- Java/src/InsertionSort.java
+++ Java/src/InsertionSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void insertionSort(int[] arr){
-        for(int i = 1 ; i< arr.length-1; i++){
+        for(int i = 1 ; i< arr.length; i++){
             int j = i;
             while(j>0 && arr[j] < arr[j-1]){
                 int temp = arr[j];
