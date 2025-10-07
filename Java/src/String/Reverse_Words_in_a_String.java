1591a99d961a721e484f31bb46a53219e0377c55
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-10-07 12:37:32 +0530

    Reverse Words in a String

---
+++ Java/src/String/Reverse_Words_in_a_String.java
@@ -0,0 +1,26 @@
+package String;
+
+public class Reverse_Words_in_a_String {
+    public static String reverseWords(String s) {
+        String[] arr = s.split("\\s+");
+        int st = 0 , end = arr.length-1;
+        while (st<end){
+            String temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+            st++;
+            end--;
+        }
+        return String.join(" ",arr).trim();
+
+    }
+
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverseWords(s));
+    }
+}
+//Example 1:
+//
+//Input: s = "the sky is blue"
+//Output: "blue is sky the"
