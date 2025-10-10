5c82d1bf9254bdb84f9e4546bafb8994c3740552
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-10-10 13:02:45 +0530

    Valid Perfect Square

---
+++ Java/src/BinarySearch/Valid_Perfect_Square.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class Valid_Perfect_Square {
+    public boolean isPerfectSquare(int x) {
+        int st = 0 , end = x;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(mid*mid == x)return true;
+            else if(mid*mid<x){
+                st = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Valid_Perfect_Square validPerfectSquare = new Valid_Perfect_Square();
+        int num = 16;
+        System.out.println(validPerfectSquare.isPerfectSquare(num));
+    }
+}
