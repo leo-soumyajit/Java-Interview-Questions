62137af6281538f058ed9e662584e0023b932895
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-10-11 14:32:39 +0530

    Valid Palindrome

---
+++ Java/src/BinarySearch/ValidPalindrome.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int st = 0 , end = s.length()-1;
+        while (st<=end){
+            if(s.charAt(st)!=s.charAt(end)){
+                return false;
+            }
+            else{
+                st++;
+                end--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+}
+//Example 1:
+//
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
